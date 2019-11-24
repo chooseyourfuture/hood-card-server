9eb9f7b7f5dcee4357f6360cd7b5bc96f456b744
AulisL <aulis.leinonen@prezella.com>
2019-11-24 12:05:33 +0200

    Specify styling for the card

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+  <nuxt />
 </template>
 
 <script>
 export default {}
 </script>
+
+<style lang="scss">
+#__nuxt {
+  width: 100%;
+}
+
+.hoods-preview {
+  height: 100vh;
+}
+</style>
